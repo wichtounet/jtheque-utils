a33e5e03c16d879a8235b4d8aaf53facf973b92f
wichtounet <baptistewicht@gmail.com>
2010-06-26 18:05:29 +0200

    Add a new EqualsBuilder class Improve performances of WebUtils Make Version Serializable Ignore IML project file

---
+++ src/main/java/org/jtheque/utils/bean/EqualsBuilder.java
@@ -0,0 +1,8 @@
+package org.jtheque.utils.bean;
+
+/**
+ * Created by IntelliJ IDEA. User: wichtounet Date: Jun 26, 2010 Time: 12:32:15 PM To change this template use File |
+ * Settings | File Templates.
+ */
+public class EqualsBuilder {
+}
