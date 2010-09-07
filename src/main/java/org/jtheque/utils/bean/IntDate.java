7649bfe2b403ab645f24d4de513b56ca756900b2
wichtounet <baptistewicht@gmail.com>
2010-09-07 21:14:47 +0200

    Review today date to provide equality test in Calendar

--- src/main/java/org/jtheque/utils/bean/IntDate.java
+++ src/main/java/org/jtheque/utils/bean/IntDate.java
@@ -181,7 +181,17 @@ public final class IntDate implements Serializable, Comparable<IntDate> {
      * @return The today IntDate
      */
     public static IntDate today() {
-        return new IntDate(Calendar.getInstance());
+        Calendar calendar = Calendar.getInstance();
+
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        calendar.clear();
+
+        calendar.set(year, month, day);
+
+        return new IntDate(calendar);
     }
 
     /**
